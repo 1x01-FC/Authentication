faacc6772bcd413828f5b29d79d1a9cae9467180
Frederic C <toonvesper@gmail.com>
2022-11-15 18:26:43 -0500

    Level 3: Hashing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const request = require("postman-request");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,10 +20,6 @@ const userSchema = new mongoose.Schema({
     email: { type: String },
     password: { type: String },
 })
-
-//a1)Encryption
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 //b)Model
 const User = mongoose.model("User", userSchema); 
@@ -47,7 +44,7 @@ app.post('/register', (req, res) =>{
     //c)CreateDocuments
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err, result) => err ? 
@@ -58,7 +55,7 @@ app.post('/register', (req, res) =>{
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, (err, i) => {
         if (err) {
